1e816ca73a2dc39b2b74e97419105ba6f877e68d
dawabar <elmore.matthew@gmail.com>
2022-03-20 17:04:50 -0400

    Final Version for Submission

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -7,36 +7,30 @@ public class CashRegister {
 	public static void main(String[] args) {
 
 //		Create Scanner for user inputs
-//		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 //		This is an introductory statement explaining what the application does.
-//		System.out.println("Enter the cost of an item, the currency tendered, and receive back the number and denomination of currency to be returned in change.");
-//		System.out.println();
+		System.out.println("Enter the cost of an item, the currency tendered, and receive back the number and denomination of currency to be returned in change.");
+		System.out.println();
 		
 //		User Story #1
 //		The user is prompted asking for the price of the item.
 		double cost = 0;
-//		System.out.print("Enter the cost of the item: ");
-//		cost = sc.nextDouble();
-		cost = Math.round(Math.random()*2000)* .01;
-//		TODO: Remove next line after testing
-		System.out.println(cost);
+		System.out.print("Enter the cost of the item: ");
+		cost = sc.nextDouble();
 
 //		Clean out the Scanner
-//		sc.nextLine();
+		sc.nextLine();
 		
 		
 //		User Story #2
 //		The user is then prompted asking how much money was tendered by the customer.
 		double tender = 0;
-//		System.out.print("Enter the amount tendered: ");
-//		tender = sc.nextDouble();
-		tender = Math.round((Math.random() + 1) * 2000) * .01;
-//		TODO: Remove next line after testing
-		System.out.println(tender);
+		System.out.print("Enter the amount tendered: ");
+		tender = sc.nextDouble();
 		
-//		TODO: Remove next line after testing;
-		System.out.println(tender - cost);
+//		Clean out the Scanner
+		sc.nextLine();
 		
 //		User Story #3
 //		Display an appropriate message if the customer provided too little money or the exact amount.
@@ -46,7 +40,7 @@ public class CashRegister {
 			System.out.println("The amount tendered is exactly the cost of the item.  No change will be tendered.");
 		}
 
-		//		User Story #4
+//		User Story #4
 //		If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer
 		
 		//This code uses cascading modulus to reduce the amount evaluated through each denomination, large to small.
@@ -54,7 +48,7 @@ public class CashRegister {
 		//Extra math required to fix double type divison precision errors in the change.
 		
 		//Refactored to use whole numbers and then convert back to coinage.  This is easier than trying to deal with decimals.
-		
+		//This code would be more DRY with an array, but those have not been taught in the course yet.
 		
 		else if (tender > cost) {
 			
@@ -62,66 +56,39 @@ public class CashRegister {
 			
 //			Remove the number of $10 bills from the remainder
 			int tenBills = (int)((diff - (diff % 1000))/1000);
-//			int tenBills = (int)((diff - (diff % 10.0))/10);
 			diff -= tenBills * 1000;
-////			TODO: Remove next line
-//			System.out.println("tenBills: " + tenBills);
-//			System.out.println(diff);
 			
 //			Remove the number of $5 bills from the remainder
 			int fiveBills = (int)((diff - (diff % 500))/500);
-//			int fiveBills = (int)((diff - (diff % 5.0))/5.0);
 			diff -= fiveBills * 500;
-////			TODO: Remove next line
-//			System.out.println("fiveBills: " + fiveBills);
-//			System.out.println(diff);
 			
 //			Remove the number of $1 bills from the remainder
 			int oneBills = (int)((diff - (diff % 100))/100);
-//			int oneBills = (int)((diff - (diff % 1.0)));
 			diff -= oneBills * 100;
-////			TODO: Remove next line
-//			System.out.println("oneBills: " + oneBills);
-//			System.out.println(diff);
 			
 //			Remove the number of Quarters from the remainder
 			int quarters = (int)((diff - (diff % 25))/25);
-//			int quarters = (int)((diff - (diff % 0.25))/0.25);
 			diff -= quarters * 25;
-////			TODO: Remove next line
-//			System.out.println("Quarters: " + quarters);
-//			System.out.println(diff);
 			
 //			Remove the number of Dimes from the remainder
 			int dimes = (int)((diff - (diff % 10))/10);
-//			int dimes = (int)((diff - (diff % 0.10))/10);
 			diff -= dimes * 10;
-////			TODO: Remove next line
-//			System.out.println("Dimes: " + dimes);
-//			System.out.println(diff);
 			
 //			Remove the number of Nickels from the remainder
 			int nickels = (int)((diff - (diff % 5))/5);
-//			int nickels = (int)((diff - (diff % 0.05))/0.05);
 			diff -= nickels * 5;
-////			TODO: Remove next line
-//			System.out.println("Nickels: " + nickels);
-//			System.out.println(diff);
 			
 //			Remove the number of Pennies that are left in the remainder
 			int pennies = (int)(diff);
-//			int pennies = (int)(Math.round(diff * 100));
 			diff -= pennies;
-////			TODO: Remove next line
-//			System.out.println("Pennies: " + pennies);
-////			TODO: Remove next ilne after testing;
-//			System.out.println(tender - cost);
-			
-//		}
-//
-//			if (diff > 0.0) {
+
+//			Set a visual breakpoint between inputs and outputs so that the user can tell the difference.
 			System.out.println("-----------------------");
+			System.out.println("Change is $" + (tender - cost));
 			System.out.println("Denomination returned: ");
+			
+//			Now we print out the change to be returned, as long as the amount is greater than zero.
+//			This code would by more DRY with an array, but these have not been taught in the course yet.
 			
 			if (tenBills > 0) {
 				System.out.println("$10 x " + tenBills);
@@ -144,11 +111,11 @@ public class CashRegister {
 			}
 			if (pennies > 0) {
 				System.out.println(" 1\u00A2 x " + pennies);
-//				}
 			}
+
 		}
 
-//		sc.close;
+		sc.close();
 
 	}
 
